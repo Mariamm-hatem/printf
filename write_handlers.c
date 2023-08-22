228348849cfe8627bb67e8d4c643037696a52599
Mariamm-hatem <hatemmariam48@gmail.com>
2023-08-22 03:39:04 -0700

    m

--- write_handlers.c
+++ write_handlers.c
@@ -1,18 +1,4 @@
 #include "main.h"
-/************************* WRITE HANDLE *************************/
-
-/**
- * handle_write_char - Prints a string
- * @c: char types.
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
- * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
- *
- * Return: Number of chars printed.
- */
-
 int handle_write_char(char c, char buffer[],
 
 int flags, int width, int precision, int size)
@@ -22,26 +8,17 @@ int flags, int width, int precision, int size)
 int i = 0;
 
 char padd = ' ';
-
-
-
 UNUSED(precision);
 
 UNUSED(size);
-
-
 
 if (flags & F_ZERO)
 
 padd = '0';
 
-
-
 buffer[i++] = c;
 
 buffer[i] = '\0';
-
-
 
 if (width > 1)
 
@@ -52,8 +29,6 @@ buffer[BUFF_SIZE - 1] = '\0';
 for (i = 0; i < width - 1; i++)
 
 buffer[BUFF_SIZE - i - 2] = padd;
-
-
 
 if (flags & F_MINUS)
 
@@ -68,26 +43,9 @@ return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
 write(1, &buffer[0], 1));
 
 }
-
-
-
 return (write(1, &buffer[0], 1));
 
 }
-
-/************************* WRITE NUMBER *************************/
-/**
- * write_number - Prints a string
- * @is_negative: Lista of arguments
- * @ind: char types.
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
- *
- * Return: Number of chars printed.
- */
 
 int write_number(int is_negative, int ind, char buffer[],
 
@@ -99,11 +57,7 @@ int length = BUFF_SIZE - ind - 1;
 
 char padd = ' ', extra_ch = 0;
 
-
-
 UNUSED(size);
-
-
 
 if ((flags & F_ZERO) && !(flags & F_MINUS))
 
@@ -121,8 +75,6 @@ else if (flags & F_SPACE)
 
 extra_ch = ' ';
 
-
-
 return (write_num(ind, buffer, flags, width, precision,
 
 length, padd, extra_ch));
@@ -138,8 +90,6 @@ int length, char padd, char extra_c)
 {
 
 int i, padd_start = 1;
-
-
 
 if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 
@@ -226,31 +176,20 @@ int flags, int width, int precision, int size)
 
 {
 
-/* The number is stored at the bufer's right and starts at position i */
-
 int length = BUFF_SIZE - ind - 1, i = 0;
 
 char padd = ' ';
-
-
-
 UNUSED(is_negative);
 
 UNUSED(size);
-
-
 
 if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 
 return (0); /* printf(".0d", 0)  no char is printed */
 
-
-
 if (precision > 0 && precision < length)
 
 padd = ' ';
-
-
 
 while (precision > length)
 
@@ -261,28 +200,16 @@ buffer[--ind] = '0';
 length++;
 
 }
-
-
-
 if ((flags & F_ZERO) && !(flags & F_MINUS))
 
 padd = '0';
-
-
-
 if (width > length)
 
 {
 
 for (i = 0; i < width - length; i++)
-
 buffer[i] = padd;
-
-
-
 buffer[i] = '\0';
-
-
 
 if (flags & F_MINUS) /* Asign extra char to left of buffer [buffer>padd]*/
 
